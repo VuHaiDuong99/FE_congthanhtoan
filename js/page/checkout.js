138fea7fc6b4c6a4e4e37c2ec9f344c0825be947
vhduong <vhduong@FRESHER-21>
2021-02-22 11:23:55 +0700

    update giao diện

--- js/page/checkout.js
+++ js/page/checkout.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
-    $('#ecom_payment').click(function () {
+    $('#ecom-payment').click(function () {
+        alert('thanh toan được chọn');
+    })
+    $('#ecom-paymentChange').click(function () {
         alert('thanh toan được chọn');
     })
     /* $('#dialog').hide();*/
