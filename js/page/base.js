966d9bffc750c6d9aa349ad42fd84eda96a3db5c
vhduong <vhduong@FRESHER-21>
2021-02-23 08:29:02 +0700

    add option bank

--- js/page/base.js
+++ js/page/base.js
@@ -5,11 +5,20 @@
 
     initEvent() {
         let me = this;
-        
+        let nameBank = "";
+
         $(".payment-list").on("click", "li", function () {
             $(this).parent('ul').find('li div').removeClass('border-red');
             $(this).find("div").addClass('border-red');
+
+            // Lấy tên của ngân hàng
+            nameBank = $(this).find("div").attr("id");
         })
+
+       /* $(".payment-list").on("click", "li", function () {
+            $(this).parent('ul').find('li div').removeClass('border-red');
+            $(this).find("div").addClass('border-red');
+        })*/
 
         $(".btn-access").click(function () {
             $(".payment").css("display", "none");
